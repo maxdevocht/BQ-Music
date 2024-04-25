8f91c9e7bde2ea9fe9b266dda876367d79ab79fc
Max de Vocht <maxdevocht@hotmail.com>
2024-04-25 17:54:40 +0200

    Added work component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Work from './components/Work'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Header />
+      <Work />
     </>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Work() {
+    return (
+        <section className="section">
+            <div className="section--label">
+                <h2 id="work" className="label">Recent Werk</h2>
+            </div>
+        </section>
+    )
+}
