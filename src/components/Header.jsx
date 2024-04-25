1eeae8176987367e4168c431259ad64c2a3140c5
Max de Vocht <maxdevocht@hotmail.com>
2024-04-25 17:45:55 +0200

    Added backdrop filter to nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,10 @@ export default function Header() {
                     <h1><a href="#">BQ Music.</a></h1>
                 </div>
                 <h2>Specialist in custom gitaareffecten</h2>
-                <p>_Interesse? 
-                    <a href="mailto:info@bqmusic.nl">Get in touch</a>
-                </p>
+                <p>_Interesse? <a href="mailto:info@bqmusic.nl">Get in touch</a></p>
             </div>
             <div className="bio">
-                <p>Hey 👋, mijn naam is Bas Becu. Sinds 2012 is BQ Music dé plek voor unieke geluiden. Ontdek vintage gitaareffecten voorbij prijzige replica's. <br />Mijn focus? Spot-on replica's en op maat gemaakte meesterwerken tegen een redelijke prijs. Specialiteiten? Maatwerk kabelbomen en betaalbare passieve toonregeling. Ontdek jouw unieke geluid bij BQ Music!</p>
+                <p>Hey 👋, mijn naam is Bas Becu. Sinds 2012 is BQ Music dé plek voor unieke geluiden. Ontdek vintage gitaareffecten voorbij prijzige replica's. <br />Mijn focus? Spot-on replica's en op maat gemaakte meesterwerken tegen een redelijke prijs. Specialiteiten? Maatwerk kabelbomen en passieve toonregeling. Ontdek jouw unieke geluid bij BQ Music!</p>
             </div>
         </header>
     )
