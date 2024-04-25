686576084bd8bd64024e5f30a5c8bfe4ed52ebf8
Max de Vocht <maxdevocht@hotmail.com>
2024-04-25 17:35:50 +0200

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="title">
+                <div className="logo">
+                    <img src="/logo.svg" alt="BQ logo"/>
+                    <h1><a href="#">BQ Music.</a></h1>
+                </div>
+                <h2>Specialist in custom gitaareffecten</h2>
+                <p>_Interesse? 
+                    <a href="mailto:info@bqmusic.nl">Get in touch</a>
+                </p>
+            </div>
+            <div className="bio">
+                <p>Hey 👋, mijn naam is Bas Becu. Sinds 2012 is BQ Music dé plek voor unieke geluiden. Ontdek vintage gitaareffecten voorbij prijzige replica's. <br />Mijn focus? Spot-on replica's en op maat gemaakte meesterwerken tegen een redelijke prijs. Specialiteiten? Maatwerk kabelbomen en betaalbare passieve toonregeling. Ontdek jouw unieke geluid bij BQ Music!</p>
+            </div>
+        </header>
+    )
+}
