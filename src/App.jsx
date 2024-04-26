71b2e42e07fa1889f079df47b5a709d2ebf2be80
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 12:57:48 +0200

    Added review component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Work from './components/Work'
+import Reviews from './components/Reviews'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Header />
       <Work />
+      <Reviews />
     </>
   )
 }
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Reviews() {
+    return (
+        <section className="section">
+            <div className="section--label">
+                <h2 id="reviews" className="label">Reviews</h2>
+            </div>
+        </section>
+    )
+}
