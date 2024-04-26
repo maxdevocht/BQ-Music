e7338a6df96cee630978a9268a6a0e3c1b09be12
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 12:43:45 +0200

    added img

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,26 +10,42 @@ export default function Work() {
             </div>
 
             <Splide className="splide" hasTrack={false}
-            options={ {
-                rewind: true,
-                gap   : '1rem',
-            } }
-            aria-label="My Favorite Images"
-            >
-                <SplideTrack className="slider">
-                    <SplideSlide>
-                        <h1>Title 1</h1>
-                        <img src="/1.svg" alt="Image 1"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <h1>Title 2</h1>
-                        <img src="/2.svg" alt="Image 2"/>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <h1>Title 3</h1>
-                        <img src="/3.svg" alt="Image 3"/>
-                    </SplideSlide>
-                </SplideTrack>
+                options={ {
+                    rewind: true,
+                    gap   : '1rem',
+                } }
+                aria-label="My Favorite Images"
+                >
+                    <SplideTrack className="slider">
+                        <SplideSlide>
+                            <h1 className="card__title">SansAmp Overdrive & Aguilar TLC Compressor</h1>
+                            <img src="/4.svg" alt="Image 1"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">Jazzbass X-blend Wiring</h1>
+                            <img src="/13.svg" alt="Image 2"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">Custom-made PreAmps</h1>
+                            <img src="/17.svg" alt="Image 3"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">Brooks Bass; Master Volume and Dual Output</h1>
+                            <img src="/20.svg" alt="Image 3"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">POT Overdrive; based on the King of Tone</h1>
+                            <img src="/21.svg" alt="Image 3"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">SWUNG Guitar Wiring</h1>
+                            <img src="/27.svg" alt="Image 3"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <h1 className="card__title">Wiring Harness</h1>
+                            <img src="/27.svg" alt="Image 3"/>
+                        </SplideSlide>
+                    </SplideTrack>
 
             </Splide>
         </section>
