080f0a0e3599407a5774afeff6aa671c1a6e88c7
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 13:16:05 +0200

    review styling

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,6 +6,19 @@ export default function Reviews() {
             <div className="section--label">
                 <h2 id="reviews" className="label">Reviews</h2>
             </div>
+
+            <div class="quote">
+                    <h3>"Uitstekende verkoper! De V12 is een briljant ontworpen toonregeling! Het supernette soldeerwerk is zo'n genot om naar te kijken dat het bijna jammer is dat je het achter een slagplaat moet verbergen."</h3>
+                    <h4>Tjarko - Etsy</h4>
+                </div>
+                <div class="quote">
+                    <h3>"My Twelve String Brooks Bass has a Master volume control. Plus two output jacks. Each out is dedicated to its own pickup. But when you take out the bridge output jack, the signal of both pickups goes to the remaining output. A very clever solution by BQ Music!"</h3>
+                    <h4>Rob van den Broek - The Gibson Bass Book</h4>
+                </div>
+                <div class="quote">
+                    <h3>"De nieuwe bas elektronica overtrof mijn verwachtingen. Wat een mooie kwaliteit en het soldeerwerk van Bas is echt uitmuntend. Een absolute aanrader!"</h3>
+                    <h4>Danny - Etsy</h4>
+                </div>
         </section>
     )
 }
