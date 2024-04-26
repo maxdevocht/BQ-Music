e55f39671bd61f47335cb5e06d39e2f6e96245f3
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 11:57:42 +0200

    Added images

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 export default function Work() {
     return (
@@ -6,6 +8,30 @@ export default function Work() {
             <div className="section--label">
                 <h2 id="work" className="label">Recent Werk</h2>
             </div>
+
+            <Splide className="splide" hasTrack={false}
+            options={ {
+                rewind: true,
+                gap   : '1rem',
+            } }
+            aria-label="My Favorite Images"
+            >
+                <SplideTrack className="slider">
+                    <SplideSlide>
+                        <h1>Title 1</h1>
+                        <img src="/1.svg" alt="Image 1"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <h1>Title 2</h1>
+                        <img src="/2.svg" alt="Image 2"/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <h1>Title 3</h1>
+                        <img src="/3.svg" alt="Image 3"/>
+                    </SplideSlide>
+                </SplideTrack>
+
+            </Splide>
         </section>
     )
 }
