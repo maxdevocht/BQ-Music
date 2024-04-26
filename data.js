f5993dc603279ad6f1b82a35ec0ef33732a1f884
Max de Vocht <maxdevocht@hotmail.com>
2024-04-26 11:59:14 +0200

    Created data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        id: 1,
+        title: "SansAmp Overdrive & Aguilar TLC Compressor",
+    },
+
+    {
+        id: 2,
+        title: "Jazzbass X-blend Wiring",
+    },
+
+    {
+        id: 3,
+        title: "Custom-made PreAmps",
+    },
+
+    {
+        id: 4,
+        title: "Brooks Bass; Master Volume and Dual Output",
+    },
+
+    {
+        id: 5,
+        title: "POT Overdrive; based on the King of Tone",
+    },
+
+    {
+        id: 6,
+        title: "SWUNG Guitar Wiring",
+    },
+
+    {
+        id: 7,
+        title: "Wiring Harness",
+    },
+]
